577b829a3c232e4b16b6872ab37e4f9ef3633286
Robert Rodriguez <robertorb21@gmail.com>
2017-06-16 15:30:16 -0500

    Html template

---
+++ src/main/java/com/devopsbuddy/DebopsbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebopsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebopsbuddyApplication.class, args);
+	}
+}
