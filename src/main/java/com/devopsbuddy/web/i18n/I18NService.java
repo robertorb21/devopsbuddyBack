3ac32e113abb8fd43d1abc1b371330f045b05f0f
Robert Rodriguez <robertorb21@gmail.com>
2018-06-14 20:41:14 -0500

    Adding log

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,5 +1,8 @@
 package com.devopsbuddy.web.i18n;
 
+import com.oracle.tools.packager.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -12,24 +15,33 @@ import java.util.Locale;
  */
 @Service
 public class I18NService {
-    @Autowired
-    private MessageSource messageSource;
 
-    /**
-     * Returns a message for the given message id and the default locale in the session context
-     * @param messageId The key to the messages resource file
-     */
-    public String getMessage(String messageId) {
-        Locale locale = LocaleContextHolder.getLocale();
-        return getMessage(messageId, locale);
-    }
+  /**
+   * The applciation logger (from Preferences -> live template [made by RobertRodriguez])
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
 
-    /**
-     * Returns a message for the given message id and locale
-     * @param messageId The key to the message resource file
-     * @param locale The locale
-     */
-    public String getMessage(String messageId, Locale locale) {
-        return messageSource.getMessage(messageId, null, locale);
-    }
+  @Autowired
+  private MessageSource messageSource;
+
+  /**
+   * Returns a message for the given message id and the default locale in the session context
+   *
+   * @param messageId The key to the messages resource file
+   */
+  public String getMessage(String messageId) {
+    LOG.info("<<RRB>> Returning i18n text for messageId {}", messageId);
+    Locale locale = LocaleContextHolder.getLocale();
+    return getMessage(messageId, locale);
+  }
+
+  /**
+   * Returns a message for the given message id and locale
+   *
+   * @param messageId The key to the message resource file
+   * @param locale    The locale
+   */
+  public String getMessage(String messageId, Locale locale) {
+    return messageSource.getMessage(messageId, null, locale);
+  }
 }
--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -17,7 +17,8 @@ public class DevopsbuddyApplicationTests {
 
     @Test
     public void testMessageByLocaleService() throws Exception {
-        String expectedResult = "Bootstrap starter template =D";
+        //String expectedResult = "Bootstrap starter template =D";
+        String expectedResult = "Bootstrap starter template";
         String messageId = "index.main.callout";
         String actual = i18NService.getMessage(messageId);
         Assert.assertEquals("The actual and expected String don't match", expectedResult, actual);
